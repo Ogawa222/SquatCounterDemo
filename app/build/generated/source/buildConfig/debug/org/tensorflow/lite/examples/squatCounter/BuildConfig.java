737bf8858e92543a3602ed4a3d75e1efe107df59
Ogawa222 <72852366+Ogawa222@users.noreply.github.com>
2021-11-13 20:58:51 +0900

    アプリ名の変更に伴う微修正

--- app/build/generated/source/buildConfig/debug/org/tensorflow/lite/examples/squatCounter/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/org/tensorflow/lite/examples/squatCounter/BuildConfig.java
@@ -5,11 +5,7 @@ package org.tensorflow.lite.examples.squatCounter;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-<<<<<<< HEAD:app/build/generated/source/buildConfig/debug/org/tensorflow/lite/examples/posenet/BuildConfig.java
-  public static final String APPLICATION_ID = "org.squat.counter.examples.posenet";
-=======
   public static final String APPLICATION_ID = "org.tensorflow.lite.examples.squatCounter";
->>>>>>> 9603d47b93ebc2af68fe3b9f845f0f8108369d11:app/build/generated/source/buildConfig/debug/org/tensorflow/lite/examples/squatCounter/BuildConfig.java
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
